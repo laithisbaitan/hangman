7b237ac90c511666d82d99b3237627220e6937cf
laithisbaitan <80163581+laithisbaitan@users.noreply.github.com>
2023-11-08 01:41:12 +0200

    implemented good and bad guessses, and tests

--- hangman/lib/impl/game.ex
+++ hangman/lib/impl/game.ex
@@ -1,9 +1,10 @@
 defmodule Hangman.Impl.Game do
+
   alias Hangman.Type
 
   @type t :: %__MODULE__{
     turns_left: integer,
-    game_state: Type.state,
+    game_state: Hangman.state,
     letters:    list(String.t),
     used:       MapSet.t(String.t),
   }
@@ -15,9 +16,9 @@ defmodule Hangman.Impl.Game do
     used:       MapSet.new()
   )
 
-  ########################################################
+  ##################################################
 
-  @spec new_game() :: t
+  @spec new_game :: t
   def new_game do
     new_game(Dictionary.random_word)
   end
@@ -29,11 +30,11 @@ defmodule Hangman.Impl.Game do
     }
   end
 
-  ########################################################
+  ##################################################
 
-  @spec make_move(t, String.t) :: {t, Type.tally}
-  def make_move(game = %{game_state: state}, _guess)
-  when state in [:won, :lost] do
+  @spec make_move(t, String.t) :: { t, Type.tally }
+  def make_move(game = %{ game_state: state }, _guess)
+  when state in [ :won, :lost ] do
     game
     |> return_with_tally()
   end
@@ -43,7 +44,7 @@ defmodule Hangman.Impl.Game do
     |> return_with_tally()
   end
 
-  ########################################################
+  ##################################################
 
   defp accept_guess(game, _guess, _already_used = true) do
     %{ game | game_state: :already_used }
@@ -51,20 +52,48 @@ defmodule Hangman.Impl.Game do
 
   defp accept_guess(game, guess, _already_used) do
     %{ game | used: MapSet.put(game.used, guess) }
+    |> score_guess(Enum.member?(game.letters, guess))
   end
 
-  ########################################################
-  defp return_with_tally(game) do
-    {game, tally(game)}
+  ##################################################
+
+  defp score_guess(game, _good_guess = true) do
+    new_state = maybe_won(MapSet.subset?(MapSet.new(game.letters), game.used))
+    %{ game | game_state: new_state }
   end
+
+  defp score_guess(game = %{ turns_left: 1 }, _bad_guess) do
+    %{ game | game_state: :lost, turns_left: 0 }
+  end
+
+  defp score_guess(game, _bad_guess) do
+    %{ game | game_state: :bad_guess, turns_left: game.turns_left - 1 }
+  end
+
+  ##################################################
 
   defp tally(game) do
     %{
       turns_left: game.turns_left,
       game_state: game.game_state,
-      letters: [],
+      letters: reveal_guessed_letters(game),
       used: game.used |> MapSet.to_list |> Enum.sort
     }
   end
 
+  defp return_with_tally(game) do
+    { game, tally(game) }
+  end
+
+  defp reveal_guessed_letters(game) do
+    game.letters
+    |> Enum.map(fn letter -> MapSet.member?(game.used, letter)
+    |> maybe_reveal(letter) end)
+  end
+
+  defp maybe_won(true), do: :won
+  defp maybe_won(_),    do: :good_guess
+
+  defp maybe_reveal(true, letter),  do: letter
+  defp maybe_reveal(_,    _letter), do: "_"
 end
