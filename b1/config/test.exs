7360b3a170c24116aece976ea2acfca2a3653faa
laithisbaitan <80163581+laithisbaitan@users.noreply.github.com>
2023-11-17 23:05:37 +0200

    generated pheonix app

---
+++ b1/config/test.exs
@@ -0,0 +1,14 @@
+import Config
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :b1, B1Web.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "geR6alYXMfyv9XukZnypLnheefi0Y9ss2eZw2bt1g/20x8nlTOSXkCJ4MtlUQuif",
+  server: false
+
+# Print only warnings and errors during test
+config :logger, level: :warning
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
